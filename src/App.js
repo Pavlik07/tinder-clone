6b61a1dac525d69307dc930d338836037012bba1
Pavlik07 <pavelmos0707@gmail.com>
2020-09-09 15:02:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import TinderCards from './TinderCards';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I am chat</h1>
+          </Route>
+          <Route path="/">
+            <TinderCards />
+          </Route>
+          <Route path="/">
+            <h1>I am homepage</h1>
+          </Route>
+        </Switch>
+
+        {/*Tinder Cards
+            Buttons below
+
+            Chats screen
+            Individual ChatScreen*/}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
